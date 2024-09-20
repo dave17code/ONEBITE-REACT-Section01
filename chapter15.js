bbbab2fa137d572e37f31f7a273ad67e7a872e1b
dave17code <dave17code@gmail.com>
2024-09-21 08:57:05 +0900

    chapter14

---
+++ chapter15.js
@@ -0,0 +1,14 @@
+// 1. 객체 생성
+let obj1 = new Object(); // 객체 생성자
+let obj2 = {}; // 객체 리터럴
+
+// 2. 객체 프로퍼티 (객체 속성)
+let person = {
+    name: "이정환",
+    age: 27,
+    hobby: "테니스",
+    job: "FE Developer",
+    extra: {},
+    10 : 20,
+    "like cat": true,
+};
